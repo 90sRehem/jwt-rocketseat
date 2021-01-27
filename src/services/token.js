ab748c52f87a519d7908e867ed5bb086fc57e1dd
Jonathan <jonathan.de.oliveira@live.com>
2021-01-27 20:25:26 -0300

    verify with public key + token expite time

--- src/services/token.js
+++ src/services/token.js
@@ -3,12 +3,13 @@ const { crypto: config } = require('../../config')
 
 const signOptions = {
   algorithm: 'RS256',
+  expiresIn: '1s',
 }
 
 const sign = payload => jwt.sign(payload, config.jwt.privateKey, signOptions)
 
 const verify = token => new Promise((resolve, reject) =>
-  jwt.verify(token, config.jwt.privateKey, (error, data) =>
+  jwt.verify(token, config.jwt.publicKey, (error, data) =>
     error ? reject(error) : resolve(data)
   )
 )
